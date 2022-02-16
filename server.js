917e9146f10aa0150ccf3a5278969db839be2a26
TCL git <tuckerlarrabee13@gmail.com>
2022-02-16 15:17:40 -0500

    Connected to DB via server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Connect to DB
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username
+        user: "root",
+        //your mysql password
+        password: "Gococks17$",
+        database: "election"
+    },
+    console.log('connected to the election database')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
